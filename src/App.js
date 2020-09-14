7d1d53d2b2d60db32ef50ab34447e6b45204c728
anwachuku15 <anwachuku15@gmail.com>
2020-09-14 11:48:51 -0400

    log in -> conditional rendering with token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-import Login from './components/Login'
+import Login from "./components/Login";
+import { getTokenFromRedirectURLResponse } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  // Run code based on given conditions (dependencies)
+  useEffect(() => {
+    // keep an eye on window location & remove access token from URL
+    const hash = getTokenFromRedirectURLResponse();
+    window.location.hash = "";
+
+    // Best practice for avoiding duplicate const names
+    const _token = hash.access_token;
+    if (_token) {
+      // store in state
+      setToken(_token);
+    }
+    console.log("Token 👉", hash);
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {/* JSX: Javascript inside return */}
+      {token ? (
+        // <Player />
+        <h1>Logged In</h1>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -16,6 +16,20 @@ export const scopes = [
   "user-modify-playback-state",
 ];
 
+export const getTokenFromRedirectURLResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      // LEARN MORE ABOUT REDUCE FUNCTION
+      // #accessToken=myaccesstokenhere&token_type=Bearer&expires_in=3600
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
