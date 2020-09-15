4b313f060ac7f83234afd0ef58b1ca900c434b40
anwachuku15 <anwachuku15@gmail.com>
2020-09-15 12:48:51 -0400

    added token and user data to Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 import Login from "./components/Login";
-import { getTokenFromRedirectURLResponse } from "./spotify";
+import Player from "./components/Player";
+import { useStateValue } from "./StateProvider";
+import { getTokenFromRedirectURLResponse, spotify } from "./spotify";
 
 function App() {
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
+  const [state, dispatch] = useStateValue();
 
   // Run code based on given conditions (dependencies)
   useEffect(() => {
@@ -16,21 +19,29 @@ function App() {
     // Best practice for avoiding duplicate const names
     const _token = hash.access_token;
     if (_token) {
-      // store in state
-      setToken(_token);
+      // store token in context
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      // pass token to Spotify
+      spotify.setAccessToken(_token);
+
+      // get User response then shoot it into the context (data layer)
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
     }
-    console.log("Token 👉", hash);
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="app">
       {/* JSX: Javascript inside return */}
-      {token ? (
-        // <Player />
-        <h1>Logged In</h1>
-      ) : (
-        <Login />
-      )}
+      {state.token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
