02c2983387d9e50b41c3e2fd15fd6317dc095199
anwachuku15 <anwachuku15@gmail.com>
2020-09-16 17:25:53 -0400

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../css/Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { spotify } from "../spotify";
+import { Avatar } from "@material-ui/core";
+import { useStateValue } from "../StateProvider";
+
+const Header = () => {
+  const [{ user }, dispatch] = useStateValue();
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input
+          className="search"
+          type="text"
+          placeholder="Search for Artists, Songs, or Podcasts"
+          style={{ borderColor: "transparent" }}
+        />
+      </div>
+
+      <div className="header__right">
+        <Avatar src={user?.images[0]?.url} alt="AN" />
+        <h4>Andrew Nwachuku</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
