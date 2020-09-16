f87829e7c6e4cffba7ae058ea1890defdfac98fa
anwachuku15 <anwachuku15@gmail.com>
2020-09-16 17:27:27 -0400

    conditional styling

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "../css/SidebarOption.css";
-// import {} from "@";
 
-const SidebarOption = ({ option, Icon }) => {
+import { useStateValue } from "../StateProvider";
+
+const SidebarOption = ({ option, id, Icon }) => {
+  const [state] = useStateValue();
+
   return (
-    <div className="sidebarOption">
+    <div
+      className={
+        state.selectedPlaylistId === id
+          ? "sidebarOptionActive"
+          : "sidebarOption"
+      }
+    >
       {Icon && <Icon className="sidebarOption__icon" />}
       {Icon ? <h5>{option}</h5> : <p className="optionText">{option}</p>}
     </div>
