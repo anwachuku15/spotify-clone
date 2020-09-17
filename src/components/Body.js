6f6d1953c90742db1f0b45481cc59fbba016bc84
anwachuku15 <anwachuku15@gmail.com>
2020-09-17 00:56:20 -0400

    improved playlist ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,42 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../css/Body.css";
-import Header from "./Header";
-
+// import Header from "./Header";
+import SongRow from "./SongRow";
 import { useStateValue } from "../StateProvider";
+import { Favorite, MoreHoriz, PlayCircleFilled } from "@material-ui/icons";
+import * as Vibrant from "node-vibrant";
 
 const Body = () => {
   const [state] = useStateValue();
 
-  const selectedPlaylist = state.playlists.items
-    ? state.playlists.items.find(
-        (playlist) => playlist.id === state.selectedPlaylistId
-      )
-    : null;
+  const playlist = {
+    info: state.playlistInfo,
+    tracks: state.playlistTracks,
+  };
+
+  const [palette, setPalette] = useState({});
+
+  useEffect(() => {
+    if (state.playlistInfo) {
+      Vibrant.from(state.playlistInfo.image)
+        .getPalette()
+        .then((palette) => {
+          setPalette({
+            vibrant: palette.Vibrant.hex,
+            darkVibrant: palette.DarkVibrant.hex,
+            darkMuted: palette.DarkMuted.hex,
+            lightVibrant: palette.LightVibrant.hex,
+          });
+        })
+        .then(() => {
+          console.log(palette);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [state]);
 
   return (
     <div className="body">
       {/* <Header /> */}
 
-      {selectedPlaylist ? (
-        <div className="playlistInfoContainer">
-          {selectedPlaylist.images.length ? (
-            <img
-              src={selectedPlaylist.images[0].url}
-              alt=""
-              className="playlist_img"
-            />
-          ) : null}
-          <div className="playlistInfo">
-            {selectedPlaylist && <strong className="type">PLAYLIST</strong>}
-            <h2>{selectedPlaylist.name}</h2>
-            {selectedPlaylist.description && (
-              <p className="description">{selectedPlaylist.description}</p>
-            )}
-            <p className="owner">{selectedPlaylist.owner.display_name}</p>
+      {state.playlistInfo ? (
+        <div>
+          <div
+            className="playlistInfoContainer"
+            style={{
+              background: `linear-gradient(${palette.vibrant}, rgb(34, 34, 34))`,
+            }}
+          >
+            {playlist.info.image ? (
+              <img src={playlist.info.image} alt="" className="playlist_img" />
+            ) : null}
+            <div className="playlistInfo">
+              {playlist && <strong className="type">PLAYLIST</strong>}
+              <h2>{playlist.info.name}</h2>
+              {playlist.info.description && (
+                <p className="description">{playlist.info.description}</p>
+              )}
+              <span>
+                <p className="owner">{playlist.info.owner} </p>
+                <p className="stats">
+                  {" "}
+                  · {playlist.info.followers} likes · {playlist.info.duration}
+                </p>
+              </span>
+            </div>
+          </div>
+
+          <div className="playlist__icons">
+            <PlayCircleFilled fontSize="large" className="play" />
+            <Favorite fontSize="large" className="favorite" />
+            <MoreHoriz fontSize="large" className="more" />
+          </div>
+
+          <div className="playlist__tracks">
+            {playlist?.tracks.map((track) => (
+              <SongRow track={track} />
+            ))}
           </div>
         </div>
       ) : (
-        <h1>Welcome To Spotify</h1>
+        <h1>Welcome To SpotifyClone</h1>
       )}
     </div>
   );
