9f552cacf8f07158148193064be12626ee816046
anwachuku15 <anwachuku15@gmail.com>
2020-09-18 14:29:59 -0400

    home button logic

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "../css/Sidebar.css";
 import SidebarOption from "./SidebarOption";
-// import { useStateValue } from "../StateProvider";
+import { useStateValue } from "../StateProvider";
 import { Home, Search, LibraryMusic } from "@material-ui/icons";
 import Playlists from "./Playlists";
 
 const Sidebar = () => {
-  // const [state, dispatch] = useStateValue();
-  // console.log(state);
+  const [state, dispatch] = useStateValue();
+
+  const goHome = () => {
+    console.log("HOME");
+    dispatch({
+      type: "SET_PLAYLIST",
+      playlistInfo: null,
+      playlistTracks: null,
+    });
+  };
+
   return (
     <div className="sidebar">
       <div className="navigation">
@@ -16,7 +25,7 @@ const Sidebar = () => {
           src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
           alt=""
         />
-        <SidebarOption Icon={Home} option="Home" />
+        <SidebarOption Icon={Home} option="Home" handleClick={goHome} />
         <SidebarOption Icon={Search} option="Search" />
         <SidebarOption Icon={LibraryMusic} option="Your Library" />
 
