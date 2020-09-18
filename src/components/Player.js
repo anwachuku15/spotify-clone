053a0de0d98f9dcde4ba84c1c54316602ce60a98
anwachuku15 <anwachuku15@gmail.com>
2020-09-17 21:20:58 -0400

    spacebar press console log

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,10 +3,23 @@ import "../css/Player.css";
 import Sidebar from "./Sidebar";
 import Body from "./Body";
 import Footer from "./Footer";
+import { useStateValue } from "../StateProvider";
 
 const Player = ({ spotify }) => {
+  const [state] = useStateValue();
+
+  // const playlist = {
+  //   info: state.playlistInfo,
+  //   tracks: state.playlistTracks,
+  // };
+
+  const onSpacebar = (key) => {
+    if (key === " ") {
+      console.log(state.isPlaying);
+    }
+  };
   return (
-    <div className="player">
+    <div className="player" onKeyDown={(e) => onSpacebar(e.key)} tabIndex={0}>
       <div className="player__body">
         <Sidebar />
         <Body spotify={spotify} />
