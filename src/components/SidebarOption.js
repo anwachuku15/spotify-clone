7b50169183dfcad7eae18a75500d46db75838791
anwachuku15 <anwachuku15@gmail.com>
2020-09-18 14:29:45 -0400

    introducing logic for home/search/library buttons

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -3,7 +3,7 @@ import "../css/SidebarOption.css";
 
 import { useStateValue } from "../StateProvider";
 
-const SidebarOption = ({ option, id, Icon }) => {
+const SidebarOption = ({ option, id, Icon, handleClick }) => {
   const [state] = useStateValue();
   const activePlaylistId = state.playlistInfo ? state.playlistInfo.id : 0;
   return (
@@ -11,6 +11,7 @@ const SidebarOption = ({ option, id, Icon }) => {
       className={
         activePlaylistId === id ? "sidebarOptionActive" : "sidebarOption"
       }
+      onClick={() => (handleClick ? handleClick() : {})}
     >
       {Icon && <Icon className="sidebarOption__icon" />}
       {Icon ? <h5>{option}</h5> : <p className="optionText">{option}</p>}
