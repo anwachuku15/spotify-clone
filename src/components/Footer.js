cae487111bfafedbf9095ecd6ac257b68c9e39c2
anwachuku15 <anwachuku15@gmail.com>
2020-09-20 16:13:23 -0400

    cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -191,7 +191,7 @@ const Footer = () => {
         return playlistData;
       })
       .then((playlistData) => {
-        console.log(playlistData.playlistInfo);
+        // console.log(playlistData.playlistInfo);
         dispatch({
           type: "SET_PLAYLIST",
           playlistInfo: playlistData.playlistInfo,
