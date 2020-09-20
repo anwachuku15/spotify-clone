4030fc5275436031d14b562f1fddf9f9001b1616
anwachuku15 <anwachuku15@gmail.com>
2020-09-20 16:13:55 -0400

    active song row styling

--- src/components/SongRow.js
+++ src/components/SongRow.js
@@ -15,7 +15,7 @@ const SongRow = ({ track }) => {
   };
 
   const playTrack = (_track) => {
-    console.log(_track);
+    // console.log(_track);
     spotify
       .play({
         context_uri: _track.context_uri,
@@ -36,16 +36,38 @@ const SongRow = ({ track }) => {
         setIsPlaying(true);
       });
   };
+
   return (
     <div
       className="songRow"
-      onClick={() => console.log(track)}
+      onClick={() => console.log("keep row highlighted")}
       onDoubleClick={() => playTrack(track)}
     >
       <Grid container spacing={2} alignItems="center">
+        {/* <Grid item xs={1}>
+          <div className="rowNumber">
+            
+          </div>
+        </Grid> */}
         <Grid item xs={7}>
           <div className="songRow__title">
-            <img src={track.cover} alt="" />
+            {track.uri === state.track.uri && state.isPlaying && (
+              <img
+                className="rowNumberActive"
+                width={14}
+                height={14}
+                src="https://open.scdn.co/cdn/images/equaliser-animated-green.73b73928.gif"
+                alt=""
+              />
+            )}
+            {track.uri === state.track.uri && !state.isPlaying && (
+              <p className="rowNumberActive">{track.position + 1}</p>
+            )}
+            {track.uri !== state.track.uri && (
+              <p className="rowNumber">{track.position + 1}</p>
+            )}
+
+            <img src={track.cover} alt="" className="albumArt" />
             <div className="songRow__song">
               <span>{track.song}</span>
               <p>{track.artists.map((artist) => artist.name).join(", ")}</p>
